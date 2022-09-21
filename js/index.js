95f9995ee4e44a174238271961a1eabf93d77466
Alessandra Scarpellini <scaalessandra@icloud.com>
2022-09-21 21:31:17 +0200

    refactoring and some styling

--- js/index.js
+++ js/index.js
@@ -9,15 +9,16 @@ class Game {
         this.pointsDisplayer = null;
         this.points = 0;
         this.gameOver = null;
+        this.level = {level: "1", }
     };
     start(){
-        this.player = new Player();
-        this.pointsDisplayer = new Points();
+        this.player = new Player(player.width, player.heigth, player.positionX, player.positionY, player.img, player.id, player.icon, player.speed);
+        this.pointsDisplayer = new Points(points.width, points.heigth, points.positionX, points.positionY, points.img, points.id,  points.icon);
         this.displayPoints(this.points)
         this.playerEventListener()
 
         setInterval(() => {
-            const newPigeons = new Pigeons();
+            const newPigeons = new Pigeons(pigeon.width, pigeon.heigth, pigeon.positionX, pigeon.positionY, pigeon.img, pigeon.id, pigeon.icon, pigeon.speed, pigeon.point);
             this.pigeons.push(newPigeons);
         }, 2000);
 
@@ -50,7 +51,7 @@ class Game {
         document.addEventListener('keydown', (event) => {
             this.player.move(event.key);
             if(event.key === ' '){
-                const newBullets = new Bullets(this.player.positionX + (this.player.width / 2) , this.player.positionY + this.player.heigth);
+                const newBullets = new Bullets(bullets.width, bullets.heigth, this.player.positionX + (this.player.width / 2), this.player.positionY + this.player.heigth , bullets.img, bullets.id, bullets.icon, bullets.speed, bullets.point );
                 this.bullets.push(newBullets);
             }
         })
@@ -70,7 +71,7 @@ class Game {
                 this.pigeons.splice(pigeonIndex,1)
                 this.bullets.splice(index, 1);
                 bullet.divElm.remove()
-                const newPoop = new Poops(releasePositionX , releasePositionY);              
+                const newPoop = new Poops(poops.width, poops.heigth, releasePositionX, releasePositionY , poops.img, poops.id, poops.icon, poops.speed, poops.point );              
                 this.poops.push(newPoop)                
             }
         })            
@@ -103,7 +104,7 @@ class Game {
     displayPoints(point){
         this.points += point;
 
-        this.pointsDisplayer.divElm.innerHTML = `<h3>Points: ${this.points}<h3> <h3> Level: 1<h3>`;
+        this.pointsDisplayer.divElm.innerHTML = `<h3>Points: ${this.points}<h3> <h4> Level: ${this.level.level}<h4>`;
     };
     removePigeonOut(pigeon){  
         if((pigeon.positionX + pigeon.width)  >= 100){
@@ -127,63 +128,48 @@ class Game {
 };
 
 class GameElement {
-    constructor(width, height, positionX, positionY, color, speed){
+    constructor(width, heigth, positionX, positionY, img, id, icon, speed){
         this.width = width;
-        this.heigth = height;
+        this.heigth = heigth;
         this.positionX = positionX
         this.positionY = positionY;
         this.divElm = null;
-        this.color = color
+        this.img = img
+        this.id = id;
+        this.icon = icon;
+        this.speed = speed;
+    };
+
+    createDivElm(){
+        const parentElm = document.querySelector('#board');
+        this.divElm = document.createElement('div');
+        this.divElm.classList = this.id;
+        this.divElm.innerHTML = this.icon;
+        this.divElm.style.backgroundImage = this.img;
+        this.divElm.style.width = this.width + "%";
+        this.divElm.style.height = this.heigth + "%";
+        this.divElm.style.bottom = this.positionY + "%";
+        this.divElm.style.left = this.positionX + "%"
+        parentElm.appendChild(this.divElm);
+    };
+};
+
+class Player extends GameElement {
+    constructor(width, heigth, positionX, positionY, img, id,icon, speed){
+        super(width, heigth, positionX, positionY, img, id,icon, speed)
+        this.width = width;
+        this.heigth = heigth;
+        this.positionX = positionX
+        this.positionY = positionY;
+        this.divElm = null;
+        this.img = img;
+        this.id = id;
         this.speed = speed;
         this.createDivElm();
     };
 
-    createDivElm(){
-        const parentElm = document.querySelector('#board');
-        this.divElm = document.createElement('div');
-        this.divElm.id = 'player';
-       // this.divElm.style.backgroundColor = this.color
-        this.divElm.style.width = this.width + "%";
-        this.divElm.style.height = this.heigth + "%";
-        this.divElm.style.bottom = this.positionY + "%";
-        this.divElm.style.left = this.positionX + "%"
-
-        parentElm.appendChild(this.divElm);
-    };
-
-}
-
-
-
-class Player {
-    constructor(){
-        this.width = 12;
-        this.heigth = 16;
-        this.positionX = 45
-        this.positionY = 0;
-        this.divElm = null;
-        this.color = 'blue'
-        this.speed = 3;
-        this.createDivElm();
-    };
-
-    createDivElm(){
-        const parentElm = document.querySelector('#board');
-        this.divElm = document.createElement('div');
-        this.divElm.id = 'player';
-       // this.divElm.style.backgroundColor = this.color
-        this.divElm.style.width = this.width + "%";
-        this.divElm.style.height = this.heigth + "%";
-        this.divElm.style.bottom = this.positionY + "%";
-        this.divElm.style.left = this.positionX + "%"
-
-        parentElm.appendChild(this.divElm);
-    };
-
     move(key){
         switch(key){
-
-        
             case 'ArrowLeft' :              
                 if(this.positionX > 0 && this.positionX <= (100 - this.width)){
                    this.positionX -= this.speed; 
@@ -191,9 +177,7 @@ class Player {
                     this.positionX = 0;
                 }
                 this.divElm.style.left = this.positionX + "%";
-                break;
-            
-        
+                break;       
             case 'ArrowRight' :                       
                 if(this.positionX >= 0 && this.positionX < (100 - this.width)){
                     this.positionX += this.speed; 
@@ -201,41 +185,23 @@ class Player {
                     this.positionX = 100 - this.width;
                 } 
                 this.divElm.style.left = this.positionX + "%"; 
-                break;
-                            
+                break;                            
         }
     }     
-        
-    
-
-
-
 };
 
-class Pigeons {
-    constructor(){
-        this.width = 6;
-        this.heigth = 6;
-        this.positionX = 0;
-        this.positionY = 70;
+class Pigeons extends GameElement{
+    constructor(width, heigth, positionX, positionY, img, id, icon, speed, point){
+        super(width, heigth, positionX, positionY, img, id, icon, speed,)
+        this.width = width;
+        this.heigth = heigth;
+        this.positionX = positionX;
+        this.positionY = positionY;
         this.divElm = null;
-        this.color = 'grey'
-        this.speed = 0.4;
-        this.point = 5;
+        this.img = img;
+        this.speed = speed;
+        this.point = point;
         this.createDivElm();
-    };
-    
-    createDivElm(){
-        const parentElm = document.querySelector('#board');
-        this.divElm = document.createElement('div');
-        this.divElm.classList = 'pigeons';
-        //this.divElm.style.backgroundColor = this.color
-        this.divElm.style.width = this.width + "%";
-        this.divElm.style.height = this.heigth + "%";
-        this.divElm.style.bottom = this.positionY + "%";
-        this.divElm.style.left = this.positionX + "%"
-
-        parentElm.appendChild(this.divElm);
     };
 
     move(){
@@ -244,91 +210,61 @@ class Pigeons {
         }
 };
 
-class Bullets {
-    constructor(positionX, positionY){
-        this.width = 1;
-        this.heigth = 1;
+class Bullets extends GameElement{
+    constructor(width, heigth, positionX, positionY, img, id, icon, speed){
+        super(width, heigth, positionX, positionY, img, id, icon, speed)
+        this.width = width;
+        this.heigth = heigth;
         this.positionX = positionX;
         this.positionY = positionY;
         this.divElm = null;
-        //this.color = 'grey'
-        this.speed = 1;
+        this.img = img
+        this.id = id;
+        this.icon = icon;
+        this.speed = speed;
         this.createDivElm();
     };
-    
-    createDivElm(){
-        const parentElm = document.querySelector('#board');
-        this.divElm = document.createElement('div');
-        this.divElm.classList = 'bullets';
-        this.divElm.innerHTML = '<i class="fa-solid fa-bomb"></i>';
-        //this.divElm.style.backgroundColor = this.color;
-        this.divElm.style.width = this.width + "%";
-        this.divElm.style.height = this.heigth + "%";
-        this.divElm.style.bottom = this.positionY  + "%";
-        this.divElm.style.left = this.positionX + "%";
 
-        parentElm.appendChild(this.divElm);
-    };
     shoot(){
         this.positionY += this.speed;
         this.divElm.style.bottom = this.positionY + "%";
     }
 };
 
-class Poops {   
-    constructor(positionX, positionY){
-        this.width = 2;
-        this.heigth = 2;
+class Poops extends GameElement{   
+    constructor(width, heigth, positionX, positionY, img, id, icon, speed){
+        super(width, heigth, positionX, positionY, img, id, icon, speed)
+        this.width = width;
+        this.heigth = heigth;
         this.positionX = positionX;
         this.positionY = positionY;
         this.divElm = null;
-       // this.color = 'brown'
-        this.speed = 1;
+        this.img = img;
+        this.id = id;
+        this.icon = icon;
+        this.speed = speed;
         this.createDivElm();
     };
     
-    createDivElm(){
-        const parentElm = document.querySelector('#board');
-        this.divElm = document.createElement('div');
-        this.divElm.classList = 'poops';
-        this.divElm.innerHTML = '<i class="fa-solid fa-poo"></i>'
-        //this.divElm.style.backgroundColor = this.color;
-        this.divElm.style.width = this.width + "%";
-        this.divElm.style.height = this.heigth + "%";
-        this.divElm.style.bottom = this.positionY  + "%";
-        this.divElm.style.left = this.positionX + "%";
-
-        parentElm.appendChild(this.divElm);
-    };
     fall(){
         this.positionY -= this.speed;
         this.divElm.style.bottom = this.positionY + "%";
     }
 };
 
-class Points {
-    constructor(){
-        this.width = 24;
-        this.height = 18;
-        this.positionX = 110;
-        this.positionY = 40;
+class Points extends GameElement{
+    constructor(width, heigth, positionX, positionY, img, id, icon){
+        super(width, heigth, positionX, positionY, img, id, icon)
+        this.width = width;
+        this.height = heigth;
+        this.positionX = positionX;
+        this.positionY = positionY;
         this.divElm = null;
-        this.color = 'orange'
+        this.img = img;
+        this.id = id;
+        this.icon = icon;
         this.createDivElm();       
-    };
-    createDivElm(){
-        const parentElm = document.querySelector('#board');
-        this.divElm = document.createElement('div');
-        this.divElm.id = 'points';
-        this.divElm.style.background = this.color
-        this.divElm.style.width = this.width + "%";
-        this.divElm.style.height = this.heigth + "%";
-        this.divElm.style.bottom = this.positionY  + "%";
-        this.divElm.style.left = this.positionX + "%";
-
-        parentElm.appendChild(this.divElm);
-    };
-
+    }; 
 };
 
 class GameOver {
@@ -346,6 +282,12 @@ class GameOver {
         parentElm.appendChild(this.divElm);
     };
 };
+
+const player = {width: 12, heigth: 18, positionX: 45, positionY: 0, img: "url('../img/old1.png')", id:"player", icon:null, speed: 3};
+const pigeon = {width: 6, heigth: 6, positionX: 0, positionY: 70, img: "url('../img/pigeon-fly.png')", id:"pigeons", icon: null, speed: 0.4, point:5};
+const bullets = {width: 1, heigth: 1, positionX: null, positionY: null, img: null, id: 'bullets', icon: '<i class="fa-solid fa-bomb"></i>', speed: 1};
+const poops = {width: 2, heigth: 2, positionX: null, positionY: null, img: null, id: 'poops', icon: '<i class="fa-solid fa-poo"></i>', speed: 1}
+const points = {width: 24, heigth: 56, positionX: 110, positionY: 20, img: "url('../img/pigeon-board.jpeg')", id: 'points' , icon: null}
 
 
 const game = new Game();
